4e6136fcea3f7fcc06c8821b41543e2acdad54d7
VARSHA <VARSHA@LAPTOP-1Q0P1P7K>
2026-08-18 20:01:17 +0530

    "Add all Java programs"

---
+++ src/ab/dmeo.java
@@ -0,0 +1,18 @@
+package ab;
+
+public class dmeo {
+	void minbal() {
+		System.out.println("sdfa");
+	}
+	void minbal(int a) {
+	
+		System.out.println("dfasdfasdfasfas");
+	}
+	public static void main(String[] args) {
+		dmeo test = new dmeo();// object
+		test.minbal();
+		test.minbal(3);
+	}
+
+
+}
