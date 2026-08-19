59ea3177658bfcea838fd407e11567e5e01f91ef
VARSHA <VARSHA@LAPTOP-1Q0P1P7K>
2026-08-19 20:14:26 +0530

    Add all java program

---
+++ d/src/d/test.java
@@ -0,0 +1,18 @@
+package d;
+
+public class test {
+	int a = 20;
+	int b = 10;
+
+	void m1(int c,int d) {
+		System.out.println("dfasdf "+(a+b));
+	System.out.println("asdfas"+(c+d));
+	}
+
+	public static void main(String[] args) {
+		test   vv = new test();
+		vv.m1(4,3);
+	}
+
+
+}
